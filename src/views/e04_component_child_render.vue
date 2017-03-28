aa49c03fe9ab51c65d9b272dc8bfd3d291d069bd
changjianwang <changjianwang@cyou-inc.com>
2017-03-28 14:40:26 +0800

    用render代替ttemplate

---
+++ src/views/e04_component_child_render.vue
@@ -0,0 +1,40 @@
+<script>
+  import child from '../components/child'
+  export default {
+    data () {
+      return {
+        groceryList: [
+          { text: 'Vegetables' },
+          { text: 'Cheese' },
+          { text: 'Whatever else humans are supposed to eat' }
+        ]
+      }
+    },
+    components: {
+      child,
+      // Define a new component called todo-item
+      'todo-item': {
+        // The todo-item component now accepts a
+        // "prop", which is like a custom attribute.
+        // This prop is called todo.
+        props: ['todo'],
+        template: '<li>{{ todo.text }}</li>'
+      }
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <child my-message="hello!"></child>
+    <ol>
+      <!-- Now we provide each todo-item with the todo object    -->
+      <!-- it's representing, so that its content can be dynamic -->
+      <todo-item v-for="item in groceryList" v-bind:todo="item"></todo-item>
+    </ol>
+  </div>
+</template>
+
+<style lang="sass">
+
+</style>
