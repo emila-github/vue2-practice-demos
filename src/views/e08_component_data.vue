acaf26daa79a09f6737ab97bb87f01cd239caf5a
changjianwang <changjianwang@cyou-inc.com>
2017-03-28 18:07:40 +0800

    动态Prop

---
+++ src/views/e08_component_data.vue
@@ -0,0 +1,28 @@
+<script>
+  var data = {counter: 0}
+  export default {
+    data () {
+      return {
+      }
+    },
+    components: {
+      'simple-counter': {
+        template: '<button v-on:click="counter += 1">{{ counter }}</button>',
+        // 技术上 data 的确是一个函数了，因此 Vue 不会警告，
+        // 但是我们返回给每个组件的实例的却引用了同一个data对象
+        data: function () {
+          return data
+        }
+      }
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <h1>data是一个函数。但data是一个引用类型</h1>
+    <simple-counter></simple-counter>
+    <simple-counter></simple-counter>
+    <simple-counter></simple-counter>
+  </div>
+</template>
